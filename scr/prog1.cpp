1a401009ed70c7792535367684683416b29bc19f
bumanlos <154221544+bumanlos@users.noreply.github.com>
2023-12-22 18:37:22 +0300

    Update prog1.cpp

--- scr/prog1.cpp
+++ scr/prog1.cpp
@@ -1,83 +1,118 @@
-#include <iostream>
-#include <vector>
-#include <queue>
+#include <stdio.h>
+#include <stdbool.h>
 
-using namespace std;
+#define SIZE 8
 
-// Структура для хранения координат вершин
-struct Vertex {
-    int x;
-    int y;
-};
-
-// Функция для генерации матрицы с заданными размерами
-vector<vector<int>> generateMatrix(int rows, int cols) {
-    vector<vector<int>> matrix(rows, vector<int>(cols, 0));
-    int value = 0;
-    
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < cols; j++) {
-            matrix[i][j] = value++;
-        }
-    }
-    
-    return matrix;
+// Проверка, является ли данная вершина допустимой по координатам
+bool isValid(int x, int y) {
+return (x >= 0 && x < SIZE && y >= 0 && y < SIZE);
 }
 
-// Функция для нахождения кратчайшего пути между двумя вершинами в матрице
-int findShortestPath(const vector<vector<int>>& matrix, const Vertex& start, const Vertex& end) {
-    int rows = matrix.size();
-    int cols = matrix[0].size();
-    
-    vector<vector<int>> distance(rows, vector<int>(cols, -1));
-    queue<Vertex> q;
-    
-    // Инициализируем расстояния
-    distance[start.x][start.y] = 0;
-    q.push(start);
-    
-    while (!q.empty()) {
-        Vertex v = q.front();
-        q.pop();
-        
-        // Перебор соседних вершин
-        int dx[] = {-1, 1, 0, 0};
-        int dy[] = {0, 0, -1, 1};
-        for (int i = 0; i < 4; i++) {
-            int nx = v.x + dx[i];
-            int ny = v.y + dy[i];
-            
-            // Если соседняя вершина находится в пределах матрицы и еще не была посещена
-            if (nx >= 0 && nx < rows && ny >= 0 && ny < cols && distance[nx][ny] == -1) {
-                distance[nx][ny] = distance[v.x][v.y] + 1;
-                q.push({nx, ny});
-            }
+// Проверка, была ли данная вершина посещена
+bool isVisited(int x, int y, bool visited[SIZE][SIZE]) {
+return visited[x][y];
+}
+
+// Нахождение кратчайшего пути между двумя вершинами с помощью алгоритма Ли
+int findShortestPath(int graph[SIZE][SIZE], int startX, int startY, int endX, int endY) {
+// Проверка, являются ли начальная и конечная вершины допустимыми
+if (!isValid(startX, startY) || !isValid(endX, endY)) {
+return -1;
+}
+
+// Создание матрицы посещенных вершин, все значения инициализируются false
+bool visited[SIZE][SIZE];
+for (int i = 0; i < SIZE; i++) {
+    for (int j = 0; j < SIZE; j++) {
+        visited[i][j] = false;
+    }
+}
+
+// Определение смещений для движения вверх, вниз, влево и вправо
+int dx[] = {-1, 1, 0, 0};
+int dy[] = {0, 0, -1, 1};
+
+// Начальная вершина помечается как посещенная
+visited[startX][startY] = true;
+
+// Создание очереди для хранения координат вершин
+int queue[SIZE * SIZE * 2];
+queue[0] = startX;
+queue[1] = startY;
+
+int current, currentX, currentY;
+int front = 0, rear = 2;
+
+// Пока очередь не пустая, продолжаем поиск пути
+while (front < rear) {
+    current = queue[front];
+    currentX = queue[front++];
+    currentY = queue[front++];
+
+    // Если найден конечный путь, возвращаем его длину
+    if (currentX == endX && currentY == endY) {
+        return current;
+    }
+
+    // Иначе двигаемся во всех возможных направлениях
+    for (int i = 0; i < 4; i++) {
+        int newX = currentX + dx[i];
+        int newY = currentY + dy[i];
+
+        // Проверка, является ли новая вершина допустимой и не посещена
+        if (isValid(newX, newY) && graph[newX][newY] && !isVisited(newX, newY, visited)) {
+            visited[newX][newY] = true;
+            queue[rear++] = current + 1;
+            queue[rear++] = newX;
+            queue[rear++] = newY;
         }
     }
-    
-    return distance[end.x][end.y];
+}
+
+// Если пути нет, возвращаем -1
+return -1;
+}
+
+// Вывод графа на экран
+void printGraph(int graph[SIZE][SIZE]) {
+for (int i = 0; i < SIZE; i++) {
+for (int j = 0; j < SIZE; j++) {
+printf("%d ", graph[i][j]);
+}
+printf("\n");
+}
 }
 
 int main() {
-    int rows, cols;
-    cout << "Введите количество строк: ";
-    cin >> rows;
-    cout << "Введите количество столбцов: ";
-    cin >> cols;
-    
-    vector<vector<int>> matrix = generateMatrix(rows, cols);
-    
-    int start_x, start_y, end_x, end_y;
-    cout << "Введите координаты начальной вершины (x и y через пробел): ";
-    cin >> start_x >> start_y;
-    cout << "Введите координаты конечной вершины (x и y через пробел): ";
-    cin >> end_x >> end_y;
-    
-    Vertex start = {start_x, start_y};
-    Vertex end = {end_x, end_y};
-    
-    int shortestPath = findShortestPath(matrix, start, end);
-    cout << "Кратчайший путь между выбранными вершинами: " << shortestPath << endl;
-    
-    return 0;
+int graph[SIZE][SIZE] = {
+{1, 1, 0, 1, 1, 0, 0, 1},
+{1, 1, 0, 0, 0, 0, 0, 1},
+{0, 0, 1, 0, 0, 0, 0, 1},
+{1, 0, 0, 1, 1, 0, 0, 1},
+{1, 0, 0, 1, 1, 1, 1, 1},
+{0, 0, 0, 0, 1, 1, 0, 0},
+{0, 0, 0, 0, 1, 0, 1, 1},
+{1, 1, 1, 1, 1, 0, 1, 1}
+};
+
+// Вывод графа на экран
+printGraph(graph);
+
+int startX, startY, endX, endY;
+printf("Введите начальные координаты (x, y): ");
+scanf("%d %d", &startX, &startY);
+printf("Введите конечные координаты (x, y): ");
+scanf("%d %d", &endX, &endY);
+
+// Нахождение кратчайшего пути
+int shortestPath = findShortestPath(graph, startX, startY, endX, endY);
+
+// Вывод результата
+if (shortestPath != -1) {
+    printf("Кратчайший путь между (%d, %d) и (%d, %d) равен %d", startX, startY, endX, endY, shortestPath);
+} else {
+printf("Пути между (%d, %d) и (%d, %d) нет", startX, startY, endX, endY);
+}
+
+return 0;
 }
