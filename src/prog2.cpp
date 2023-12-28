772e0373ca504ae9acc6813e8a0aa99871780329
bumanlos <154221544+bumanlos@users.noreply.github.com>
2023-12-28 17:10:18 +0300

    Update prog2.cpp

--- src/prog2.cpp
+++ src/prog2.cpp
@@ -1,82 +1,47 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-#define ROWS 10
-#define COLS 10
-
-// Функция для печати графа
-void printGraph(int graph[ROWS][COLS]) {
-    for (int i = 0; i < ROWS; i++) {
-        for (int j = 0; j < COLS; j++) {
-            printf("%d ", graph[i][j]);
-        }
-        printf("\n");
-    }
-}
-
-// Функция для реализации алгоритма Ли
-void leeAlgorithm(int graph[ROWS][COLS], int startX, int startY, int endX, int endY) {
-    int queue[ROWS * COLS * 2]; // Очередь для хранения координат вершин
-    int front = 0, rear = 0; // Фронт и зад очереди
-    int dx[] = {-1, 0, 1, 0}; // Смещения по оси x для соседних вершин
-    int dy[] = {0, 1, 0, -1}; // Смещения по оси y для соседних вершин
-
-    queue[rear++] = startX; // Добавляем начальную вершину в очередь
-    queue[rear++] = startY;
-
-    while (front < rear) { // Пока очередь не пуста
-        int x = queue[front++]; // Извлекаем координаты вершины из очереди
-        int y = queue[front++];
-
-        if (x == endX && y == endY) { // Если достигли конечной вершины
-            printf("Кратчайший путь между вершиной (%d, %d) и вершиной (%d, %d): %d шагов\n", startX, startY, endX, endY, graph[endX][endY]);
-            return;
-        }
-
-        for (int i = 0; i < 4; i++) { // Перебираем соседние вершины
-            int nx = x + dx[i]; // Вычисляем координаты соседней вершины
-            int ny = y + dy[i];
-
-            if (nx >= 0 && nx < ROWS && ny >= 0 && ny < COLS && graph[nx][ny] == 0) { // Если соседняя вершина доступна для перемещения
-                graph[nx][ny] = graph[x][y] + 1; // Устанавливаем расстояние до неё
-                queue[rear++] = nx; // Добавляем её в очередь
-                queue[rear++] = ny;
-            }
-        }
-    }
-
-    printf("Кратчайший путь между вершиной (%d, %d) и вершиной (%d, %d) не найден\n", startX, startY, endX, endY);
-}
-
-int main() {
-    int graph[ROWS][COLS] = {
-        {0, 1, 0, 1, 0, 0, 0, 0, 0, 0},
-        {1, 0, 1, 0, 1, 0, 0, 0, 0, 0},
-        {0, 1, 0, 0, 0, 1, 0, 0, 0, 0},
-        {1, 0, 0, 0, 1, 1, 1, 0, 0, 0},
-        {0, 1, 0, 1, 0, 0, 0, 1, 0, 0},
-        {0, 0, 1, 1, 0, 0, 0, 1, 1, 0},
-        {0, 0, 0, 1, 0, 0, 0, 0, 1, 1},
-        {0, 0, 0, 0, 1, 1, 0, 0, 0, 1},
-        {0, 0, 0, 0, 0, 1, 1, 0, 0, 1},
-        {0, 0, 0, 0, 0, 0, 1, 1, 1, 0}
-    };
-
-    printf("Планарный граф:\n");
-    printGraph(graph);
-
-    int startX;
-    int startY;
-    int endX;
-    int endY;
-
-    printf("Введите начальные координаты вершины (x y): ");
-    scanf("%d %d", &startX , &startY );
-
-    printf("Введите конечные координаты вершины (x y): ");
-    scanf("%d %d", &endX , &endY );
-
-    leeAlgorithm(graph,startX , startY , endX , endY );
-
-    return 0;
+#include <stdio.h>  
+#include <unistd.h>  
+#include <stdlib.h>  
+#include <sys/types.h>  
+#include <sys/stat.h>  
+#include <ctime>  
+#include <fcntl.h>  
+#include <sys/sysinfo.h>  
+#include <sys/statvfs.h>  
+#include <sys/resource.h> 
+ 
+int main() { // Перенаправление вывода stdout в log.txt freopen("../files/log.txt", "w", stdout); 
+ 
+  
+// Отображение информации о мониторинге системы 
+printf("Мониторинг системы"); 
+ 
+  
+// Код мониторинга 
+// Отображение текущего идентификатора процесса и системного времени 
+int pid = getpid(); 
+printf("Текущий идентификатор процесса: %d", pid); time_t current_time = time(NULL);  
+printf("Текущее время: %s", ctime(&current_time)); 
+ 
+  
+// Получение информации об использовании памяти 
+struct sysinfo mem_info; 
+sysinfo(&mem_info); 
+printf("Общий объем ОЗУ: %ld МБ", mem_info.totalram / 1024 / 1024);  
+printf("Свободная ОЗУ: %ld МБ ", mem_info.freeram / 1024 / 1024); 
+ 
+  
+// Получение информации об использовании ЦП 
+struct rusage cpu_info; 
+getrusage(RUSAGE_SELF, &cpu_info); 
+printf("Использование ЦП: %ld.%06ld секунд", cpu_info.ru_utime.tv_sec, cpu_info.ru_utime.tv_usec); 
+ 
+  
+// Получение информации о дисковом пространстве 
+struct statvfs disk_info; 
+statvfs(".", &disk_info); 
+printf("Общий объем дискового пространства: %ld ГБ", (disk_info.f_blocks * disk_info.f_frsize) / 1024 / 1024 / 1024);  
+printf("Доступное дисковое пространство: %ld ГБ ", (disk_info.f_bavail * disk_info.f_frsize) / 1024 / 1024 / 1024); 
+ 
+  
+return 0; 
 }
