d5b40946226effd2f9fdd7dca0768346725756f7
bumanlos <154221544+bumanlos@users.noreply.github.com>
2023-12-28 17:06:24 +0300

    Update prog1.cpp

--- src/prog1.cpp
+++ src/prog1.cpp
@@ -4,6 +4,7 @@
 #define ROWS 10
 #define COLS 10
 
+// Функция для печати графа
 void printGraph(int graph[ROWS][COLS]) {
     for (int i = 0; i < ROWS; i++) {
         for (int j = 0; j < COLS; j++) {
@@ -13,10 +14,38 @@ void printGraph(int graph[ROWS][COLS]) {
     }
 }
 
-// Функция для поиска кратчайшего пути между двумя вершинами с помощью алгоритма Ли
-void findShortestPath(int graph[ROWS][COLS], int startVertex, int endVertex) {
-    // Здесь можно добавить код для реализации алгоритма Ли
-    printf("Кратчайший путь между вершиной %d и вершиной %d: ...\n", startVertex, endVertex);
+// Функция для реализации алгоритма Ли
+void leeAlgorithm(int graph[ROWS][COLS], int startX, int startY, int endX, int endY) {
+    int queue[ROWS * COLS * 2]; // Очередь для хранения координат вершин
+    int front = 0, rear = 0; // Фронт и зад очереди
+    int dx[] = {-1, 0, 1, 0}; // Смещения по оси x для соседних вершин
+    int dy[] = {0, 1, 0, -1}; // Смещения по оси y для соседних вершин
+
+    queue[rear++] = startX; // Добавляем начальную вершину в очередь
+    queue[rear++] = startY;
+
+    while (front < rear) { // Пока очередь не пуста
+        int x = queue[front++]; // Извлекаем координаты вершины из очереди
+        int y = queue[front++];
+
+        if (x == endX && y == endY) { // Если достигли конечной вершины
+            printf("Кратчайший путь между вершиной (%d, %d) и вершиной (%d, %d): %d шагов\n", startX, startY, endX, endY, graph[endX][endY]);
+            return;
+        }
+
+        for (int i = 0; i < 4; i++) { // Перебираем соседние вершины
+            int nx = x + dx[i]; // Вычисляем координаты соседней вершины
+            int ny = y + dy[i];
+
+            if (nx >= 0 && nx < ROWS && ny >= 0 && ny < COLS && graph[nx][ny] == 0) { // Если соседняя вершина доступна для перемещения
+                graph[nx][ny] = graph[x][y] + 1; // Устанавливаем расстояние до неё
+                queue[rear++] = nx; // Добавляем её в очередь
+                queue[rear++] = ny;
+            }
+        }
+    }
+
+    printf("Кратчайший путь между вершиной (%d, %d) и вершиной (%d, %d) не найден\n", startX, startY, endX, endY);
 }
 
 int main() {
@@ -36,16 +65,18 @@ int main() {
     printf("Планарный граф:\n");
     printGraph(graph);
 
-    int startVertex;
-    int endVertex;
+    int startX;
+    int startY;
+    int endX;
+    int endY;
 
-    printf("Введите начальную вершину (от 1 до %d): ", ROWS);
-    scanf("%d", &startVertex);
+    printf("Введите начальные координаты вершины (x y): ");
+    scanf("%d %d", &startX , &startY );
 
-    printf("Введите конечную вершину (от 1 до %d): ", ROWS);
-    scanf("%d", &endVertex);
+    printf("Введите конечные координаты вершины (x y): ");
+    scanf("%d %d", &endX , &endY );
 
-    findShortestPath(graph, startVertex - 1 , endVertex -1); // -1 чтобы привести к индексации с нуля
+    leeAlgorithm(graph,startX , startY , endX , endY );
 
     return 0;
 }
