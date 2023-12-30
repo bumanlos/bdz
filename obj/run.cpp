4926d6ac1769ec72c5b7c9c52c1487cca760c8f5
bumanlos <154221544+bumanlos@users.noreply.github.com>
2023-12-30 12:33:05 +0300

    Update run.cpp

--- obj/run.cpp
+++ obj/run.cpp
@@ -13,8 +13,8 @@ int main() {
     char *prog1 = "./prog1"; // Путь к первой программе
     char *prog2 = "./prog2"; // Путь ко второй программе
 
-    pthread_create(&thread1, NULL, thread_function, (void *)program1); // Создаем первый поток
-    pthread_create(&thread2, NULL, thread_function, (void *)program2); // Создаем второй поток
+    pthread_create(&thread1, NULL, thread_function, (void *)prog1); // Создаем первый поток
+    pthread_create(&thread2, NULL, thread_function, (void *)prog2); // Создаем второй поток
 
     pthread_join(thread1, NULL); // Ждем завершения первого потока
     pthread_join(thread2, NULL); // Ждем завершения второго потока
