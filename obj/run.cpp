96da07a7173dd9b30ef0bd6293366c6f779fce7d
bumanlos <154221544+bumanlos@users.noreply.github.com>
2023-12-30 13:20:09 +0300

    Update run.cpp

--- obj/run.cpp
+++ obj/run.cpp
@@ -1,25 +1,43 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <pthread.h>
-
-void *runProgram1(void *args) {
-    system("../src/prog1.cpp"); // Замените "program1.exe" на имя исполняемого файла первой программы
-    return NULL;
-}
-
-void *runProgram2(void *args) {
-    system("../src/prog2.cpp"); // Замените "program2.exe" на имя исполняемого файла второй программы
-    return NULL;
-}
-
-int main() {
-    pthread_t thread1, thread2;
-
-    pthread_create(&thread1, NULL, runProgram1, NULL);
-    pthread_create(&thread2, NULL, runProgram2, NULL);
-
-    pthread_join(thread1, NULL);
-    pthread_join(thread2, NULL);
-
-    return 0;
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <unistd.h> 
+#include <sys/wait.h> 
+ 
+int main() { 
+    pid_t pid = fork(); 
+    if (pid < 0) { 
+        perror("fork failed"); 
+        exit(1); 
+    } else if (pid == 0) { 
+        // Это код дочернего процесса 
+        execl("./p1", "p1", NULL); 
+        perror("execl failed"); 
+        exit(1); 
+    } else { 
+        // Это код родительского процесса 
+        int status; 
+        waitpid(pid, &status, 0); 
+        if (WIFEXITED(status)) { 
+            printf("Дочерний процесс завершился с кодом: %d\n", WEXITSTATUS(status)); 
+        } 
+         
+        pid = fork(); 
+        if (pid < 0) { 
+            perror("fork failed"); 
+            exit(1); 
+        } else if (pid == 0) { 
+            // Это код дочернего процесса 
+            execl("./p2", "p2", NULL); 
+            perror("execl failed"); 
+            exit(1); 
+        } else { 
+            // Это код родительского процесса 
+            waitpid(pid, &status, 0); 
+            if (WIFEXITED(status)) { 
+                printf("Дочерний процесс завершился с кодом: %d\n", WEXITSTATUS(status)); 
+            } 
+        } 
+    } 
+ 
+    return 0; 
 }
