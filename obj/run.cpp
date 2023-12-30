b713db63f85f261dcf8b9c2d487ab330e2d35674
bumanlos <154221544+bumanlos@users.noreply.github.com>
2023-12-30 15:02:06 +0300

    Update run.cpp

--- obj/run.cpp
+++ obj/run.cpp
@@ -130,8 +130,8 @@ void *Thread2(void *args) {
 int main() {
     pthread_t thread1, thread2;
 
-    pthread_create(&thread1, NULL, runProgram1, NULL);
-    pthread_create(&thread2, NULL, runProgram2, NULL);
+    pthread_create(&thread1, NULL, Thread1, NULL);
+    pthread_create(&thread2, NULL, Thread2, NULL);
 
     pthread_join(thread1, NULL);
     pthread_join(thread2, NULL);
