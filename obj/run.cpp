792620ada99c4ea03ce4f37ff639241ec493a454
bumanlos <154221544+bumanlos@users.noreply.github.com>
2023-12-30 13:01:26 +0300

    Update run.cpp

--- obj/run.cpp
+++ obj/run.cpp
@@ -3,18 +3,23 @@
 #include <pthread.h>
 
 void *thread_function(void *arg) {
-    char *program = (char *)arg;
-    system(program); // Запускаем программу
+    char *program_name = (char *)arg;
+    system(program_name); // Запускаем программу
     pthread_exit(NULL);
 }
 
-int main() {
-    pthread_t thread1, thread2;
-    char *prog1 = "../src/prog1.cpp -o prog1 && ./prog1"; // Путь к первой программе
-    char *prog2 = "../src/prog2.cpp -o prog2 && ./prog2"; // Путь ко второй программе
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        printf("Usage: %s <../src/prog1.cpp> <../src/prog2.cpp>\n", argv[0]);
+        return 1;
+    }
 
-    pthread_create(&thread1, NULL, thread_function, (void *)prog1); // Создаем первый поток
-    pthread_create(&thread2, NULL, thread_function, (void *)prog2); // Создаем второй поток
+    pthread_t thread1, thread2;
+    char *program1 = argv[1]; // Путь к первой программе
+    char *program2 = argv[2]; // Путь ко второй программе
+
+    pthread_create(&thread1, NULL, thread_function, (void *)program1); // Создаем первый поток
+    pthread_create(&thread2, NULL, thread_function, (void *)program2); // Создаем второй поток
 
     pthread_join(thread1, NULL); // Ждем завершения первого потока
     pthread_join(thread2, NULL); // Ждем завершения второго потока
