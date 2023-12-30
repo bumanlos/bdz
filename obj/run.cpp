8b43656daa47ccd87dce8065fc5c5329028db988
bumanlos <154221544+bumanlos@users.noreply.github.com>
2023-12-30 12:01:10 +0300

    Update run.cpp

--- obj/run.cpp
+++ obj/run.cpp
@@ -1,9 +1,9 @@
 #include <cstdlib>
 int main() {
-    // Запуск программы p1.cpp
+    // Запуск программы prog1.cpp
     system("g++ ../src/prog1.cpp -o prog1 && ./prog1");
 
-    // Запуск программы p2.cpp
+    // Запуск программы prog2.cpp
     system("g++ ../src/prog2.cpp -o prog2 && ./prog2");
 
     return 0;
