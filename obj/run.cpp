f4d2c85a7788b11eb61dcae2d7d39acddc336fcf
bumanlos <154221544+bumanlos@users.noreply.github.com>
2023-12-30 12:23:58 +0300

    Update run.cpp

--- obj/run.cpp
+++ obj/run.cpp
@@ -1,10 +1,27 @@
-#include <cstdlib>
-int main() {
-    // Запуск программы prog1.cpp
-    system("g++ ../src/prog1.cpp -o prog1 && ./prog1");
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-    // Запуск программы prog2.cpp
-    system("g++ ../src/prog2.cpp -o prog2 && ./prog2");
+int main() {
+    pid_t pid1, pid2;
+
+    pid1 = fork(); // Создаем первый дочерний процесс
+
+    if (pid1 == 0) {
+        // Этот код будет выполняться в первом дочернем процессе
+        execlp("prog1", "prog1", NULL); // Запускаем программу program1
+    }
+
+    pid2 = fork(); // Создаем второй дочерний процесс
+
+    if (pid2 == 0) {
+        // Этот код будет выполняться во втором дочернем процессе
+        execlp("prog2", "prog2", NULL); // Запускаем программу program2
+    }
+
+    // Родительский процесс ждет завершения обоих дочерних процессов
+    waitpid(pid1, NULL, 0);
+    waitpid(pid2, NULL, 0);
 
     return 0;
 }
