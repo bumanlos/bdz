935683ac4b50387a8618c86882db1bd745f3d4b7
bumanlos <154221544+bumanlos@users.noreply.github.com>
2023-12-30 13:04:35 +0300

    Update run.cpp

--- obj/run.cpp
+++ obj/run.cpp
@@ -1,28 +1,24 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <pthread.h>
+#include <iostream>
+#include <thread>
 
-void *thread_function(void *arg) {
-    char *program_name = (char *)arg;
-    system(program_name); // Запускаем программу
-    pthread_exit(NULL);
+void thread_function(const std::string& program_name) {
+    system(program_name.c_str()); // Запускаем программу
 }
 
 int main(int argc, char *argv[]) {
     if (argc != 3) {
-        printf("Usage: %s <../src/prog1.cpp> <../src/prog2.cpp>\n", argv[0]);
+        std::cout << "Usage: " << argv[0] << " <../src/prog1> <../src/prog2>" << std::endl;
         return 1;
     }
 
-    pthread_t thread1, thread2;
-    char *program1 = argv[1]; // Путь к первой программе
-    char *program2 = argv[2]; // Путь ко второй программе
+    std::string program1 = argv[1]; // Путь к первой программе
+    std::string program2 = argv[2]; // Путь ко второй программе
 
-    pthread_create(&thread1, NULL, thread_function, (void *)program1); // Создаем первый поток
-    pthread_create(&thread2, NULL, thread_function, (void *)program2); // Создаем второй поток
+    std::thread thread1(thread_function, program1); // Создаем первый поток
+    std::thread thread2(thread_function, program2); // Создаем второй поток
 
-    pthread_join(thread1, NULL); // Ждем завершения первого потока
-    pthread_join(thread2, NULL); // Ждем завершения второго потока
+    thread1.join(); // Ждем завершения первого потока
+    thread2.join(); // Ждем завершения второго потока
 
     return 0;
 }
