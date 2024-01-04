4a8090ad636fd1259b8e7ca7933d5530554b0ec9
bumanlos <154221544+bumanlos@users.noreply.github.com>
2024-01-04 11:41:38 +0300

    Update main.c

--- obj/main.c
+++ obj/main.c
@@ -8,8 +8,6 @@ int main() {
 
 pthread_t thread1, thread2;
 
-// Нужно запускать в pthread_create функции, а не прописывать имена файлов :)
-// На выходе, после компиляции у вас должен быть один общий файл, а не три
 pthread_create(&thread1, NULL, monitoring, NULL);
 pthread_create(&thread2, NULL, beginlee, NULL);
 
