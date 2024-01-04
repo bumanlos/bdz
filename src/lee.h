7c4e7d1fc0647c7f34fefe0fa510e324037e654a
bumanlos <154221544+bumanlos@users.noreply.github.com>
2024-01-04 11:33:13 +0300

    Update lee.h

--- src/lee.h
+++ src/lee.h
@@ -1,1 +1,8 @@
+// Для правильного разделения программы используйте заголовочные файлы
+// Почитайте про это, например, у Таненбаума или где-то ещё.
+#define ROWS 10
+#define COLS 10
 
+void* beginlee(); // Объявление функции для запуска алгоритма
+void leeAlgorithm(int graph[ROWS][COLS], int startX, int startY, int endX, int endY); // Объявление функции для реализации алгоритма Ли
+void printGraph(int graph[ROWS][COLS]); // Объявление функции для печати графа
