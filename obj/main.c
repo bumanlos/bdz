56289db73a97b444ede88483b2f7b9b2bbbd07cd
bumanlos <154221544+bumanlos@users.noreply.github.com>
2024-01-05 18:37:29 +0300

    Update main.c

--- obj/main.c
+++ obj/main.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
-#include "monitoring.h"
-#include "lee.h"
+#include "../src/monitoring.h" // Подключаем заголовочный файл для мониторинга
+#include "../src/lee.h" // Подключаем заголовочный файл для алгоритма Ли
 
 int main() {
+    pthread_t thread1, thread2; // Объявляем переменные для идентификаторов потоков
 
-pthread_t thread1, thread2;
+    // Создаем поток для мониторинга
+    pthread_create(&thread1, NULL, monitoring, NULL);
 
-pthread_create(&thread1, NULL, monitoring, NULL);
-pthread_create(&thread2, NULL, beginlee, NULL);
+    // Создаем поток для выполнения алгоритма Ли
+    pthread_create(&thread2, NULL, beginlee, NULL);
 
-pthread_join(thread1, NULL);
-pthread_join(thread2, NULL);
+    // Ожидаем завершения работы потоков
+    pthread_join(thread1, NULL);
+    pthread_join(thread2, NULL);
 
-return 0;
+    return 0;
 }
