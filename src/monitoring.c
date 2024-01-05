9014a802a7e430dda1a611cd0b8fc8619c445430
bumanlos <154221544+bumanlos@users.noreply.github.com>
2024-01-05 18:44:56 +0300

    Update monitoring.c

--- src/monitoring.c
+++ src/monitoring.c
@@ -10,26 +10,26 @@
 #include <sys/resource.h>
 
 void* monitoring() {
-// Описание функции monitoring
+    // Описание функции monitoring
 
-FILE* id = fopen("../files/log.txt", "w"); // Открытие файла для записи
-printf("Мониторинг системы\n"); // Вывод сообщения о начале мониторинга
+    FILE* id = fopen("../files/log.txt", "w"); // Открытие файла для записи
+    printf("Мониторинг системы\n"); // Вывод сообщения о начале мониторинга
 
-int pid = getpid(); // Получение идентификатора процесса
-fprintf(id, "Текущий идентификатор процесса: %d\n", pid); // Запись идентификатора процесса в файл
+    int pid = getpid(); // Получение идентификатора процесса
+    fprintf(id, "Текущий идентификатор процесса: %d\n", pid); // Запись идентификатора процесса в файл
 
-time_t current_time = time(NULL); // Получение текущего времени
-fprintf(id, "Текущее время: %s", ctime(&current_time)); // Запись текущего времени в файл
+    time_t current_time = time(NULL); // Получение текущего времени
+    fprintf(id, "Текущее время: %s", ctime(&current_time)); // Запись текущего времени в файл
 
-struct sysinfo mem_info; // Структура для информации о памяти
-sysinfo(&mem_info); // Получение информации о памяти
-fprintf(id, "Общий объем ОЗУ: %ld МБ\n", mem_info.totalram / 1024 / 1024); // Запись общего объема ОЗУ в файл
-fprintf(id, "Свободная ОЗУ: %ld МБ\n", mem_info.freeram / 1024 / 1024); // Запись свободной памяти в файл
+    struct sysinfo mem_info; // Структура для информации о памяти
+    sysinfo(&mem_info); // Получение информации о памяти
+    fprintf(id, "Общий объем ОЗУ: %ld МБ\n", mem_info.totalram / 1024 / 1024); // Запись общего объема ОЗУ в файл
+    fprintf(id, "Свободная ОЗУ: %ld МБ\n", mem_info.freeram / 1024 / 1024); // Запись свободной памяти в файл
 
-struct rusage cpu_info; // Структура для информации о использовании ЦП
-getrusage(RUSAGE_SELF, &cpu_info); // Получение информации об использовании ЦП
-fprintf(id, "Использование ЦП: %ld.%06ld секунд\n", cpu_info.ru_utime.tv_sec, cpu_info.ru_utime.tv_usec); // Запись информации об использовании ЦП в файл
+    struct rusage cpu_info; // Структура для информации о использовании ЦП
+    getrusage(RUSAGE_SELF, &cpu_info); // Получение информации об использовании ЦП
+    fprintf(id, "Использование ЦП: %ld.%06ld секунд\n", cpu_info.ru_utime.tv_sec, cpu_info.ru_utime.tv_usec); // Запись информации об использовании ЦП в файл
 
-fclose(id); // Закрытие файла
-return 0; // Возврат значения
+    fclose(id); // Закрытие файла
+    return 0; // Возврат значения
 }
